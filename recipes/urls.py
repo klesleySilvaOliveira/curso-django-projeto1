3374291f72bfbb7d237ab56d67f2cb54063dd9a1
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2025-05-23 20:12:02 -0300

    template reorganization and base.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<int:id>/', views.recipes, name="recipes"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name' : 'Klesley Oliveira',
+    })
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name' : 'Klesley Oliveira',
+    })

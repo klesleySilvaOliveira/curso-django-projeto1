938059b3e3a6260f72dc518026db0b97ffd31969
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2025-05-24 19:17:18 -0300

    updated templates using real models

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:id>/', views.recipes, name="recipe"),
+    path('category/<int:category_id>/', views.category, name="category"),
 ]
